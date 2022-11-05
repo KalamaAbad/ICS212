ce11df38f75fb62c5002a5541a719768078004ac
Lama <kalama.abad@gmail.com>
2022-11-04 17:15:31 -1000

    Started homework 7

--- README.md
+++
@@ -1,1 +0,0 @@
-# ICS212 Project 1.
---
+++ homework7/homework7.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <iomanip>
+
+void is_multiple4(int num, int* address);
+
+
+void is_multiple4(int num, int *ptr)
+{
+
+}
+
+int main() {
+    // 
+    std::cout << "Hello World!" << std::endl;
+    return 0;
+}
