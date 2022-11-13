e52a248fc0f3185d70cea8b77464ed5e34d11c3f
Hemotoxic <kalama.abad@gmail.com>
2022-11-12 20:50:36 -1000

    pau hana!

--- homework8/homework8.c
+++ homework8/homework8.c
@@ -2,7 +2,7 @@
 //
 //  NAME:        Kalama Abad
 //
-//  HOMEWORK:    8 
+//  HOMEWORK:    8
 //
 //  CLASS:       ICS 212
 //
@@ -13,7 +13,7 @@
 //  FILE:        homework8.c
 //
 //  DESCRIPTION: This file contains C functions for reading binary data
-//   
+//
 //
 ****************************************************************/
 
@@ -30,7 +30,7 @@ void makeheader(const unsigned char [], unsigned char []);
 //  Function name: main
 //
 //  DESCRIPTION:   Generates response headers for binary files.
-//                 
+//
 //  Parameters:    argc (int)  : Not used.
 //                 argv (char*) : Not used.
 //
@@ -43,11 +43,33 @@ int main(int argc, char const *argv[])
     unsigned char headerFromFile[20];
     const unsigned char *header;
     unsigned char responseHeader[20];
+
     readfile("request3.bin", headerFromFile);
     header = headerFromFile;
+    printf("\n============ Printing TCP header 3 ============\n");
     printheader(header);
     makeheader(header, responseHeader);
+    printf("\n============ Printing TCP response header 3 ============\n");
+    printheader(responseHeader);
     writefile("response3.bin", responseHeader);
+
+    readfile("request2.bin", headerFromFile);
+    header = headerFromFile;
+    printf("\n\n============ Printing TCP header 2 ============\n");
+    printheader(header);
+    makeheader(header, responseHeader);
+    printf("\n============ Printing TCP response header 2 ============\n");
+    printheader(responseHeader);
+    writefile("response2.bin", responseHeader);
+
+    readfile("request1.bin", headerFromFile);
+    header = headerFromFile;
+    printf("\n\n============ Printing TCP header 1 ============\n");
+    printheader(header);
+    makeheader(header, responseHeader);
+    printf("\n============ Printing TCP response header 1 ============\n");
+    printheader(responseHeader);
+    writefile("response1.bin", responseHeader);
     return 0;
 }
 
@@ -56,7 +78,7 @@ int main(int argc, char const *argv[])
 //  Function name: printheader
 //
 //  DESCRIPTION:   Prints a TCP header.
-//                 
+//
 //  Parameters:    input (const unsigned char[])  : The header to print.
 //
 //  Return values: 0 if read, -1 if not.
@@ -70,20 +92,20 @@ void printheader(const unsigned char input[])
     i = 0;
     printf("Souce port: %d\n", ((input[1]) << 8 | (input[0])));
     printf("Destination port: %d\n", ((input[3]) << 8 | (input[2])));
-    printf(("Sequence number: %lu\n"), ((input[7]) << 24 | (input[6]) << 16 | (input[5]) << 8 | (input[4])));
-    printf(("Acknowledgement number: %lu\n"), ((input[11]) << 24 | (input[10]) << 16 | (input[9]) << 8 | (input[8])));
+    printf(("Sequence number: %u\n"), ((input[7]) << 24 | (input[6]) << 16 | (input[5]) << 8 | (input[4])));
+    printf(("Acknowledgement number: %u\n"), ((input[11]) << 24 | (input[10]) << 16 | (input[9]) << 8 | (input[8])));
     flag = input[13];
     printf("Flags: ");
     while (i < 6)
     {
         if ((flag & 1))
         {
-            (i == 0) ? printf("FIN ") : printf("");
-            (i == 1) ? printf("SYN ") : printf("");
-            (i == 2) ? printf("RST ") : printf("");
-            (i == 3) ? printf("PSH ") : printf("");
-            (i == 4) ? printf("ACK ") : printf("");        
-            (i == 5) ? printf("URG ") : printf("");        
+            (i == 0) ? printf("FIN ") : 0;
+            (i == 1) ? printf("SYN ") : 0;
+            (i == 2) ? printf("RST ") : 0;
+            (i == 3) ? printf("PSH ") : 0;
+            (i == 4) ? printf("ACK ") : 0;
+            (i == 5) ? printf("URG ") : 0;
         }
         flag = flag >> 1;
         i++;
@@ -94,7 +116,7 @@ void printheader(const unsigned char input[])
 //  Function name: readfile
 //
 //  DESCRIPTION:   Reads a binary file.
-//                 
+//
 //  Parameters:    filename (const char[])  : The file to read from.
 //                 output (unsigned char[]) : Binary data will be stored here in hex format.
 //
@@ -112,7 +134,7 @@ int readfile(const char filename[], unsigned char output[])
     }
     fread(output, sizeof(unsigned char), 20, f);
     fclose(f);
-    return 0; 
+    return 0;
 }
 
 /*****************************************************************
@@ -120,7 +142,7 @@ int readfile(const char filename[], unsigned char output[])
 //  Function name: writefile
 //
 //  DESCRIPTION:   Writes a binary TCP header file.
-//                 
+//
 //  Parameters:    filename (const char[])  : The file to write to.
 //                 output (const unsigned char[]) : The header to write to the file.
 //
@@ -146,7 +168,7 @@ int writefile(const char filename[], const unsigned char input[])
 //  Function name: makeheader
 //
 //  DESCRIPTION:   Generates a response header.
-//                 
+//
 //  Parameters:    input (const unsigned char[])  : The header to generate a response to.
 //                 output (const unsigned char[]) : This is where we will store the response.
 //
@@ -157,6 +179,7 @@ int writefile(const char filename[], const unsigned char input[])
 void makeheader(const unsigned char input[], unsigned char output[])
 {
     unsigned int plusOne;
+    int i;
 
     output[0] = input[2];
     output[1] = input[3];
@@ -183,13 +206,13 @@ void makeheader(const unsigned char input[], unsigned char output[])
     output[6] = (plusOne >> 16) & 0xFF;
     output[5] = (plusOne >> 8) & 0xFF;
     output[4] = plusOne & 0xFF;
- 
+
     if ((input[13] & 2) == 2)
     {
         output[13] = 16 ^ input[13];
     }
 
-    for (int i = 14; i < 20; i++)
+    for (i = 14; i < 20; i++)
     {
         output[i] = input[i];
     }
