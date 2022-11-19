d3f2d9b91b80f763e42163bc159e8ac18d803aa6
Lama <kalama.abad@gmail.com>
2022-11-18 20:20:26 -1000

    defined some functions

---
+++ project2/llist.cpp
@@ -0,0 +1,25 @@
+#include "llist.h"
+#include "iostream"
+
+llist::llist()
+{
+    struct record *start = NULL;
+}
+
+llist::llist(char in[])
+{
+
+}
+
+llist::~llist()
+{
+
+}
+
+void llist::addRecord(int accountno, char name[], char address[])
+{
+
+}
+
+
+
